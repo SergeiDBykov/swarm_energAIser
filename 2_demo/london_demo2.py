fd91f7adf8491158f4d74ad2a8573095ae450c1e
Patrick Fu <49091150+PatrickFu0302@users.noreply.github.com>
2023-03-08 17:48:47 +0800

    Add files via upload

--- 2_demo/london_demo2.py
+++ 2_demo/london_demo2.py
@@ -17,7 +17,7 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_absolute_percentage_error
 from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
-
+from sklearn.decomposition import PCA
 import lightgbm as lgb
 
 from scripts.utils import data_path, set_mpl, read_london, add_datetime_features, get_spectrogram
@@ -29,6 +29,7 @@ import seaborn as sns
 from IPython.display import display
 
 import cufflinks as cf
+import pydeck as pdk
 
 import os
 import platform
@@ -40,7 +41,9 @@ if str(ROOT) not in sys.path:
 if platform.system() != 'Windows':
     ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
+
 set_mpl()
+
 
 #Page title
 st.set_page_config(
@@ -102,7 +105,47 @@ data_path = os.path.join(ROOT, "0_data")
 #     df_twitter.index = df_twitter.index.tz_convert(None) #remove timezone info
     
 #     return [df_std, df_tou, df_weather, df_twitter]
+#%% 新增的functions! :D
+ 
+def geomap_pydeck(lat, lon, number_units):
+    '''
+    Geomap of the specified lat and lon with indicated number_units 
+    (randomness has been added in order to spread locations of units wider)
+    '''
+    chart_data = pd.DataFrame(
+       np.random.randn(number_units, 2) / [50, 50] + [lat, lon],
+       columns=['lat', 'lon'])    
+    
+    st.pydeck_chart(pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=lat,
+            longitude=lon,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+               'HexagonLayer',
+               data=chart_data,
+               get_position='[lon, lat]',
+               radius=200,
+               elevation_scale=4,
+               elevation_range=[0, 1000],
+               pickable=True,
+               extruded=True,
+            ),
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=chart_data,
+                get_position='[lon, lat]',
+                get_color='[200, 30, 0, 160]',
+                get_radius=200,
+            ),
+        ],
+    ))
 
+#%%
 @st.cache_data
 def read_data():
     """A wrapper for data reading with st.cache_data decorator"""
@@ -156,12 +199,14 @@ def data_preparation(power_avg, weather):
     
     df_dataset = df_dataset.merge(weather['temp'], left_index=True, right_index=True)
     df_dataset['temp_roll24_mean'] = df_dataset['temp'].rolling(24).mean()
+    df_dataset['temp_roll24_std'] = df_dataset['temp'].rolling(24).std()
     
     df_holiday = timeanddate_calendar(geo_id='uk',start_year=2011,end_year=2015+1)
     df_holiday_encode = df_holiday.copy()
     df_holiday_encode[['holiday_Name', 'holiday_Type']] = df_holiday_encode[['holiday_Name', 'holiday_Type']].astype('str').apply(preprocessing.LabelEncoder().fit_transform)
     df_holiday_encode.columns = df_holiday_encode.columns+'_encode'
     df_holiday_encode = df_holiday_encode.rename(columns={'date_encode':'date'})
+    df_dataset = df_dataset.merge(df_holiday, on='date')
     df_dataset = df_dataset.merge(df_holiday_encode, on='date')
     
     #df_dataset = df_dataset.drop('date',axis=1)
@@ -186,10 +231,14 @@ def data_split(data, tp='2013-01-01', train_ratio=0.6, val_ratio=0.5):
 @st.cache_resource
 def model_train(_train): 
     """Define and train models"""
+    n_estimators = 50
+    
     traindata = _train.dropna()
     list_feat = list(traindata.select_dtypes(['int','float']).columns)
     list_feat.remove('load')
-    
+    #list_feat.remove('year')
+    #list_feat.remove('holiday_Name_encode')
+   
     ## Models for predicting 1 day
     naive_model_1day = LinearRegression()
     naive_model_1day.fit(traindata[['load_shift168']], traindata['load'])
@@ -197,7 +246,7 @@ def model_train(_train):
     LR_model_1day = LinearRegression()
     LR_model_1day.fit(traindata[list_feat], traindata['load'])
 
-    LGB_model_1day = lgb.LGBMRegressor()
+    LGB_model_1day = lgb.LGBMRegressor(n_estimators=n_estimators)
     LGB_model_1day.fit(traindata[list_feat], traindata['load'])
 
     models_1day = [naive_model_1day, LR_model_1day, LGB_model_1day]
@@ -209,7 +258,7 @@ def model_train(_train):
     LR_model_1week = LinearRegression()
     LR_model_1week.fit(traindata[list_feat].drop('load_shift24', axis=1), traindata['load'])
 
-    LGB_model_1week = lgb.LGBMRegressor()
+    LGB_model_1week = lgb.LGBMRegressor(n_estimators=n_estimators)
     LGB_model_1week.fit(traindata[list_feat].drop('load_shift24', axis=1), traindata['load'])
 
     models_1week = [naive_model_1week, LR_model_1week, LGB_model_1week]  
@@ -363,12 +412,118 @@ metrics = pd.DataFrame([R2_1day, R2_1week, MAPE_1day, MAPE_1week],
                        columns=names,
                        index=['R2_1day', 'R2_1week', 'MAPE_1day', 'MAPE_1week'])
 
+#%% Section1: Basic info
+st.markdown('## 1. Where and what is the dataset?')
+
+# geomap of the location
+st.markdown('### 1.1 Geographical location:')
+geomap_pydeck(lat=51.5, lon=-0.136, number_units=house_number)
+
+# time-series plot of the dataset with normalized values and moving avg
+st.markdown('### 1.2 Time series in the dataset:')
+dataset_plot = df_dataset[['load', 'temp']].copy()
+dataset_plot = dataset_plot.merge(df_twitter[['tweets_total']], left_index=True, right_index=True)
+dataset_plot_mov_avg = dataset_plot.rolling(24).mean()
+dataset_plot = (dataset_plot-dataset_plot.mean())/dataset_plot.std()
+dataset_plot = dataset_plot.iplot(asFigure=True, title='Normalized time series of the dataset')
+st.plotly_chart(dataset_plot)
+
+dataset_plot_mov_avg.columns = dataset_plot_mov_avg.columns+' (moving avg.)'
+#dataset_plot = pd.concat([dataset_plot, dataset_plot_mov_avg], axis=1)
+dataset_plot_mov_avg = (dataset_plot_mov_avg-dataset_plot_mov_avg.mean())/dataset_plot_mov_avg.std()
+dataset_plot_mov_avg = dataset_plot_mov_avg.iplot(asFigure=True, title='Normalized time series of the dataset (moving average)')
+st.plotly_chart(dataset_plot_mov_avg)
+
+#%% Section2: EDA
+st.markdown('## 2. Exploratory Data Analysis (EDA) and characterization')
+
+# weekly_profile
+weekly_profile = plt.figure(figsize=(10, 3))
+sns.scatterplot(data=df_dataset, x="timeofweek", y="load", alpha=0.05)
+plt.title('Weekly profiles of energy use')
+plt.ylim((df_dataset['load'].min(), df_dataset['load'].mean()+df_dataset['load'].std()*5))
+st.pyplot(weekly_profile)
+
+# Scatter plot for demand and outdoor temperature
+df_plot = df_dataset.resample('D').mean().copy()
+df_plot = df_plot[df_plot>(df_plot.mean()-df_plot.std()*3)]
+df_plot = df_plot[df_plot<(df_plot.mean()+df_plot.std()*5)]
+df_plot['weekday/weekend'] = 'weekday'
+df_plot.loc[df_plot['weekday']>4, 'weekday/weekend'] ='weekend'
+
+# Scatter plot for demand and outdoor temperature
+df_plot = df_dataset.resample('D').mean().copy()
+df_plot = df_plot[df_plot>(df_plot.mean()-df_plot.std()*3)]
+df_plot = df_plot[df_plot<(df_plot.mean()+df_plot.std()*5)]
+df_plot['weekday/weekend'] = 'weekday'
+df_plot.loc[df_plot['weekday']>4, 'weekday/weekend'] ='weekend'
+temp_vs_load = plt.figure(figsize=(10, 5))
+sns.scatterplot(x='temp', y='load', hue="weekday/weekend",
+           data=df_plot, alpha=0.5)
+plt.title('Load v.s. outdoor temperature')
+st.pyplot(temp_vs_load)
+
+# Boxplot of day types
+df_plot = df_dataset.resample('D').mean().copy()
+df_plot = df_plot[df_plot>(df_plot.mean()-df_plot.std()*3)]
+df_plot = df_plot[df_plot<(df_plot.mean()+df_plot.std()*5)]
+df_plot = df_plot.merge(df_dataset[['holiday_Type_encode','holiday_Type']],on='holiday_Type_encode')
+df_plot.loc[df_plot['holiday_Type'].str.contains('Holiday'), 'holiday_Type'] = 'Holiday'
+daytype_vs_load = plt.figure(figsize=(10, 5))
+sns.boxplot(x='holiday_Type', y='load', data=df_plot)
+plt.title('Load v.s. day types')
+st.pyplot(daytype_vs_load)
+
+# Corr bewteen load and all other variables
+dataset_plot = df_dataset[['load', 'temp']].copy()
+dataset_plot = dataset_plot.merge(df_twitter, left_index=True, right_index=True)
+dataset_plot = dataset_plot.resample('D').mean()
+
+#pca = PCA(n_components=2)
+#df_PCA = pd.DataFrame(pca.fit_transform(dataset_plot.T), columns=['PCA1', 'PCA2'], index=dataset_plot.columns)
+#PCA_fig = df_PCA.reset_index().iplot(kind='scatter',x='PCA1',y='PCA2',categories='index', 
+#                       asFigure=True, title='Distance between time series')
+#st.plotly_chart(PCA_fig)
+
+corr_dataset = dataset_plot.corr()
+if len(corr_dataset)>10: # Remove irrelevant variables if the heatmap is bigger than 10 x 10
+    corr_dataset = corr_dataset[corr_dataset['load'].abs().sort_values(ascending=False)[:10].index]
+    corr_dataset = corr_dataset.loc[corr_dataset.columns]
+corr_heatmap = plt.figure(figsize=(10, 6))
+sns.heatmap(corr_dataset, annot=True, cmap="coolwarm")
+plt.title('Correlations between variables')
+st.pyplot(corr_heatmap)
+
+corr_dataset = corr_dataset['load'].drop('load')
+corr_dataset = corr_dataset[corr_dataset.abs().sort_values(ascending=False).index]
+for row in corr_dataset.items():
+    var_name, corr_value = row
+    if corr_value > 0.6:
+        st.write('#### **Load** is highly correlated to ', '`{}`'.format(var_name), ' with correlation coeff of ', '`{}`'.format(str(round(corr_value,4))) )
+        print('Load is highly correlated to '+var_name+' with correlation coeff of ' + str(round(corr_value,4)))
+    elif corr_value < -0.6:
+        st.write('#### **Load** is highly correlated to negative', '`{}`'.format(var_name), ' with correlation coeff of ', '`{}`'.format(str(round(corr_value,4))))
+        print('Load is highly correlated to negative '+var_name+' with correlation coeff of ' + str(round(corr_value,4)))        
+
+#%% Section4: Modeling results
+st.markdown('## 4. How is the prediction result?')
+
+# Feature importance
+df_importance = pd.DataFrame(data=models_1day[-1].feature_importances_, index=list_feat, columns=['Feature importance'])
+df_importance = df_importance.sort_values('Feature importance', ascending=False)
+st.write(df_importance)
 
 # time-series plot
-fig_1day = y_test_1day.iloc[:24].iplot(asFigure=True, title='Prediction result of 1-day forecasting')
+d = st.date_input(
+    "The start day for visualizing prediction results",
+    test.iloc[0]['date'].date())
+st.write('You select:', d)
+
+fig_1day = y_test_1day.loc[d:].iloc[:24].iplot(asFigure=True, title='Prediction result of 1-day forecasting')
 st.plotly_chart(fig_1day)
-fig_1week = y_test_1week.iloc[:24*7].iplot(asFigure=True, title='Prediction result of 1-week forecasting')
+fig_1week = y_test_1week.loc[d:].iloc[:24*7].iplot(asFigure=True, title='Prediction result of 1-week forecasting')
 st.plotly_chart(fig_1week)
 
 # metrics
 st.write(metrics)
+
